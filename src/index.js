0df956aee4b26a85b08d6c51b4127387f2d27786
Salvador Peralta <salvadorperalta@MacBookProSpc.local>
2019-02-08 16:10:46 -0600

    Authentication functionallity added

--- src/index.js
+++ src/index.js
@@ -1,45 +1,26 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { prisma } = require('./generated/prisma-client')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
 
 const resolvers = {
-    Query: {
-        info: () => `This is the API of hackernews Clone change something else changed`,
-        feed: (root, args, context, info) => {
-            return context.prisma.links();
-        },
-        // link: (parent, args) => links.find((x) => {return x.id == args.id;})
-        // 
-    },
-    Mutation: {
-        post: (parent, args, context) => {
-            return context.prisma.createLink({
-                description: args.description,
-                url: args.url
-            });
-        },
-        // update : (parent, args) => {
-        //     let link = links.find((x) => {return x.id == args.id;});
-
-        //     if(link){
-        //         link.description = args.description,
-        //         link.url = args.url
-
-        //         return link;
-        //     }
-
-        //     return null;
-        // },
-        // delete: (parent, args) => {
-        //     let linkIndex = links.findIndex((x) => {return x.id == args.id;});
-        //     return links.splice(linkIndex, 1)[0];
-        // },
-    }
-}
+    Query,
+    Mutation,
+    User,
+    Link
+  }
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
-    context: { prisma }
+    context: request => {
+        return {
+            ...request,
+            prisma,
+        }
+    },
 });
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
